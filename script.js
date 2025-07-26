932c2f1f5c5cf7ca6af5cd1f63f31ffa1e1a13d4
Poovarasan R <poovarasan.mern@gmail.com>
2025-07-26 09:23:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function targetcontact(){
+  document.getElementById("contact-me").scrollIntoView();
+}
+
+const resume = () =>{
+  window.open("https://poovi-r.github.io/portfolio/Poovarasan-Resume.pdf", "_blank");
+}
