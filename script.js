2c8cbdc874a8de1a0d4d91bf6230290fa0ec411c
Poovarasan R <poovarasan.mern@gmail.com>
2025-07-26 09:31:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function targetcontact(){
-  document.getElementById("contact-me").scrollIntoView();
-}
-
-const resume = () =>{
-  window.open("https://poovi-r.github.io/portfolio/Poovarasan-Resume.pdf", "_blank");
-}
